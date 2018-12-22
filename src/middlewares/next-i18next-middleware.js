66500a9ea84e578d3022b5f3362cb4eb1e02177c
Kingsley Don <tj971204@gmail.com>
2018-12-22 20:07:04 +0800

    feat: support next-route dynamic routing

--- src/middlewares/next-i18next-middleware.js
+++ src/middlewares/next-i18next-middleware.js
@@ -2,7 +2,7 @@ import i18nextMiddleware from 'i18next-express-middleware'
 import { forceTrailingSlash, lngPathDetector } from 'utils'
 import { parse } from 'url'
 
-export default function (nexti18next, app, server) {
+export default function (nexti18next, app, server, routeMap) {
 
   const { config, i18n } = nexti18next
   const { allLanguages, localeSubpaths } = config
@@ -12,6 +12,18 @@ export default function (nexti18next, app, server) {
   if (localeSubpaths) {
     server.get('*', forceTrailingSlash)
     server.get(/^\/(?!_next|static).*$/, lngPathDetector)
+    if (Array.isArray(routeMap)) {
+      routeMap.forEach(([path, callback]) => {
+        if (typeof callback !== 'function') {
+          return
+        }
+        server.get(`(/:lng(${allLanguages.join('|')}))?${path}`, (req, res) => {
+          const [actualPath, pathQuery] = callback(req, res)
+          const { params: { lng }, query } = req
+          app.render(req, res, actualPath.replace(`/${lng}`, ''), { lng, ...pathQuery, ...query })
+        })
+      })
+    }
     server.get(`/:lng(${allLanguages.join('|')})/*`, (req, res) => {
       const { lng } = req.params
       const { query } = req
